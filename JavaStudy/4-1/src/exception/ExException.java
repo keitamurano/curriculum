d724b9ad81d36211bb9016e1a374e2f3fecebfb6
keitamurano <7banters7@gmail.com>
2022-08-27 11:23:58 +0900

    コミット

--- JavaStudy/4-1/src/exception/ExException.java
+++ JavaStudy/4-1/src/exception/ExException.java
@@ -61,7 +61,7 @@ public class ExException {
                     // 問①: 強制的に「NullPointerException」を発生させるメソッドを作成し、呼び出しなさい。
                     // 問①は最下部にもあります。
                     // ルール: ここへ作成したメソッドを呼び出す
-                    none();
+                    nullpo(null);
                     break;
                     case CONST_EXCEPTION_TRIGER_ARRAY_OUT_OF_BOUNDS:
                     // 問②: 「throw」を使用せずに「ArrayIndexOutOfBoundsException」を発生させる処理を記述しなさい。
@@ -104,9 +104,8 @@ public class ExException {
      * ルール2: 例外発生時に設定するメッセージは、定義済みの定数から適当なものを指定してください。
      */
     // ここへ記述
-    private static void none() throws NullPointerException {
-    	Scanner sc = new Scanner(System.in);
-    	System.out.println(CONST_MSG_NULLPO);
+    private static void nullpo(NullPointerException e)throws NullPointerException{
+		throw new NullPointerException(CONST_MSG_NULLPO);
     }
     	
 
---
+++ JavaStudy/Sample/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+ 
+    public static void main(String[] args) {
+        Sum sum = new Sum();
+        int sumValue = sum.plus(1, 2, 3);
+        System.out.println(sumValue);
+    }
+}
